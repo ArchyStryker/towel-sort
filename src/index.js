1f8e9d3df98e27cbbf2a6ddf28d7130c728e4538
ArchyStryker <61948618+ArchyStryker@users.noreply.github.com>
2021-03-14 14:51:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@
 
 module.exports = function towelSort (matrix) {
   return ((matrix.map((value, index) => {
-    return (index % 2 == 0 ? value : value.reverse())})flat(Infinity));
+    return (index % 2 == 0 ? value : value.reverse())
+  }).flat(Infinity));
 }
