c831078b4fa45dfb09fbdf1bd95dc000a6fb0ee6
ArchyStryker <61948618+ArchyStryker@users.noreply.github.com>
2021-03-14 14:58:52 +0300

    fix: SyntaxError

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
+module.exports = function towelSort(matrix) {
   return ((matrix.map((value, index) => {
-    return (index % 2 == 0 ? value : value.reverse())
-  }).flat(Infinity));
+    return (index % 2 === 0 ? value : value.reverse());
+  }).flat());
 }
