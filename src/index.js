b9b0594bda1527d3952b5f31888874de6fcc91e1
ArchyStryker <61948618+ArchyStryker@users.noreply.github.com>
2021-03-14 15:03:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
-  return ((matrix.map((value, index) => {
+  return (matrix.map((value, index) => {
     return (index % 2 === 0 ? value : value.reverse());
   }).flat());
 }
