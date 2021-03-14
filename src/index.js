dabda0f941a8d63d25663fb1662bbfec906e49e8
ArchyStryker <61948618+ArchyStryker@users.noreply.github.com>
2021-03-14 15:13:59 +0300

    feat: checking undefined

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
+  if (array === undefined || array.length === 0) {
+    return [];
+  }
+
   return (matrix.map((value, index) => {
     return (index % 2 === 0 ? value : value.reverse());
   }).flat());
