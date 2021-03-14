b07c401b2d4dba587162f310be029a8073457717
ArchyStryker <61948618+ArchyStryker@users.noreply.github.com>
2021-03-14 14:29:15 +0300

    feat: add reverse

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return (matrix.flat(Infinity));
+  return ((matrix.map((value, index) => index % 2 == 0 ? value : value.reverse())flat(Infinity));
 }
